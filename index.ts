e6f613b51808292f9edd4b8731399e96fee83633
Matthias <me@mattwill.be>
2020-01-05 18:35:36 +0100

    Updated versioning (#6)

--- index.ts
+++ index.ts
@@ -5,11 +5,15 @@
 import { Broker } from './libs/broker';
 import { get as config } from 'config';
 import { init as Sentry } from '@sentry/node';
+import { version } from './package.json';
+import { log } from './libs/logger';
 
 Sentry({
   dsn: config('sentry'),
-  environment: process.env.NODE_ENV || 'development',
-  release: 'farmlab-broker@' + process.env.npm_package_version
+  environment: process.env.NODE_ENV,
+  release: 'farmlab-broker@' + version
 });
+
+log.info(`Starting APP (environment: ${process.env.NODE_ENV} | version: farmlab-broker@ + ${version})`)
 
 new Broker();
